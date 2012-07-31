0f9218d78ee6b67797a70099600a738463e5da86
root <root@stocks.dyndns.org>
2012-07-30 18:12:01 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '2b659b993e1ddcccfcf6845af1beaa4ab0d6be8f555252cb23b6f0674435ef797534573c55158b2276e55eede5e56fb2302a92f6038147cf037cee5b3a81bc68'
